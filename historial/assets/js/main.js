b9c8208983eeb72babcbb8bbd5d86403312c755b
Iván <al416753@uji.es>
2025-09-23 22:36:55 +0200

    AdaptacionCasiPerfectaATodasLasPantallas

--- historial/assets/js/main.js
+++ historial/assets/js/main.js
@@ -77,9 +77,9 @@
 			xlarge:   [ '1281px',  '1680px' ],
 			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
+			small:    [ '481px',   '10px'  ],/*CAMBIOS_ROTACION 736px
 			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ],
+			xxsmall:  [ null,      '360px'  ],*/
 			short:    '(min-aspect-ratio: 16/7)',
 			xshort:   '(min-aspect-ratio: 16/6)'
 		});
